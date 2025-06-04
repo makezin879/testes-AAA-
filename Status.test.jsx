b0177a98d81c72f7ef543699c95dd89e72bdf6d5
joao jeronimo <103337815+makezin879@users.noreply.github.com>
2025-06-04 07:57:51 -0300

    Delete Status.test.jsx

--- Status.test.jsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Status from "../Status";
-
-test("Renderiza status corretamente", () => {
-  render(<Status status="entregue" />);
-  expect(screen.getByText(/Pago/i)).toBeInTheDocument();
-});
